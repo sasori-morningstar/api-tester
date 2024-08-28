7de6b925931636e3699e6ac222de49cd86e31a45
Sasori Morningstar <medjedoub.ilyes07@gmail.com>
2024-08-29 00:15:37 +0200

    Modified the guide element

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.get("/", (req, res)=>{
         message: "Welcome to Api Tester.",
         guide: {
             GET: "Please use this format: http://localhost:3000/api/request?method=GET&target={{targetWebsite}?{query}={value}<and>{query}={value}<and>...<and>{query}={value}}",
-            POST: "Please use this format: http://localhost:3000/api/request?method=POST&target={targetWebsite}&params={name}={value}<and>{name}={value}<and>...<and>{name}={value}"
+            POST: "Please use this format: http://localhost:3000/api/request?method=POST&target={targetWebsite}&params={name}={value}<and>{name}={value}<and>...<and>{name}={value}",
+            proxy: "To use proxy in your request, please add a proxy query with the url of the proxy as a value, (EX: ?proxy=protocol://name:pass@ip:port)"
         },
-        proxy: "To use proxy in your request, please add a proxy query with the url of the proxy as a value, (EX: ?proxy=protocol://name:pass@ip:port)"
     })
 })
